9620227c4c0703e089d21c6276b0045aa57c6139
diamant1991 <diamant1988@mail.ru>
2018-02-21 12:57:16 +0200

    add files

---
+++ build/js/main.js
@@ -0,0 +1,43 @@
+(function () {
+'use strict';
+
+$('.up-button').click(function (e) {
+	e.preventDefault();
+	$('body,html').animate({ scrollTop: 0 }, 800);
+});
+
+$('.carousel').slick({
+	dots: true,
+	infinite: true,
+	speed: 600,
+	fade: true,
+	cssEase: 'linear'
+});
+
+$('.hamburger-box').click(function () {
+	var menu = $('#nav');
+	if (menu.is(':hidden')) {
+		menu.show();
+		$(this).addClass('is-active');
+	} else {
+		$(this).removeClass('is-active');
+		menu.hide();
+	}
+});
+
+if (window.matchMedia('(max-width: 991px)').matches) {
+	$('.dropdown-link').click(function (e) {
+		e.preventDefault();
+		var dropdown = $(this).next();
+		if (dropdown.is(':hidden')) {
+			dropdown.show();
+			$(this).addClass('is-active');
+		} else {
+			$(this).removeClass('is-active');
+			dropdown.hide();
+		}
+	});
+}
+
+}());
+//# sourceMappingURL=main.js.map
---
+++ src/js/vendor.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
+
+import 'popper.js';
+import "bootstrap";
+import "slick-carousel";
+
